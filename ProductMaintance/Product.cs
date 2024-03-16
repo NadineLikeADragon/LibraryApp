68cda8ca3b96be6620d96a9f7f7124807aa7cdc5
NadineLikeADragon <114822966+NadineLikeADragon@users.noreply.github.com>
2024-03-16 19:15:27 +0000

    hopefully better code

---
+++ ProductMaintance/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductMaintenance
+{
+    public class Product
+    {
+         public String ProductCode { get; set; }
+        public String Description { get; set; }
+        public int OnHandQuantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string prodCode, string description, int quantity, decimal price)
+        {
+            this.ProductCode = prodCode;
+            this.Description = description;
+            this.OnHandQuantity = quantity;
+            this.UnitPrice = price;
+        }
+    }
+}
