7adc3314b53dbfc5c722fac1dc1a48f49a52014d
NadineLikeADragon <114822966+NadineLikeADragon@users.noreply.github.com>
2024-03-19 10:39:39 +0000

    Add files via upload

---
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApp/OwnedBooks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalLibraryApp
+{
+    public class OwnedBooks : Book
+    {
+        public DateTime DateBought { get; set; }
+
+        public OwnedBooks(string bookId, string title, string description, string authorLastName, DateTime dateBought)
+            : base(bookId, title, description, authorLastName)
+        {
+            DateBought = dateBought;
+        }
+
+        public OwnedBooks() { }
+    }
+}
