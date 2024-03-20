9bda520eb14a8cd9338b7c647a8a5f58c72c2930
Kerpackie <ckeleo@pm.me>
2024-03-20 17:20:31 +0000

    ReAdd AddOwnedBook Form
    ReAdds the AddOwnedBook Form.
    
    TODO: Clarify and consolidate constructors.

---
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Forms/AddOwnedBook/AddOwnedBook.cs
@@ -0,0 +1,80 @@
+﻿using PersonalLibraryApplication.Models;
+
+namespace PersonalLibraryApplication.Forms.AddOwnedBook;
+
+public partial class AddOwnedBook : Form
+    {
+        private Book book = new Book();
+        private OwnedBooks ownedBook = new OwnedBooks();
+        public AddOwnedBook()
+        {
+            InitializeComponent();
+        }
+
+        private void rbtnNewBook_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            txtDescription.Enabled = true;
+        }
+
+        private void rbtnExistingBook_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            txtDescription.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rbtnExistingBook.Checked)
+            {
+                if (ValidateForm())
+                {
+                    ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
+                    this.Hide();
+                }
+            }
+            else
+            {
+                if (ValidateForm())
+                {
+                    book = new Book(txtAuthorFName.Text, txtAuthorSName.Text, txtTitle.Text, txtDescription.Text);
+                    ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        internal OwnedBooks GetOwnedBook()
+        {
+            return ownedBook;
+        }
+
+        private bool ValidateForm()
+        {
+            if (txtTitle.Text == "")
+            {
+                MessageBox.Show("Please enter the title");
+                return false;
+            }
+            if (txtAuthorFName.Text == "" || txtAuthorSName.Text == "")
+            {
+                MessageBox.Show("Please enter the author name");
+                return false;
+            }
+            if (txtDescription.Text == "")
+            {
+                MessageBox.Show("Please describe the book");
+                return false;
+            }
+            if (dtpDateBought.Value > DateTime.Now)
+            {
+                MessageBox.Show("Please enter a valid date");
+                return false;
+            }
+            return true;
+        }
+    }
--- LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Models/OwnedBooks.cs
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Models/OwnedBooks.cs
@@ -2,11 +2,42 @@
 {
     public class OwnedBooks : Book
     {
+        bool Owned {  get; set; }
         public DateTime DateBought { get; set; }
 
+        // TODO: Ctor from old main.
         public OwnedBooks(string bookId, string title, string description, string authorLastName, DateTime dateBought)
             : base(bookId, title, description, authorLastName)
         {
+            DateBought = dateBought;
+        }
+        
+        
+        // TODO: Ctor's from Kieran Branch.
+        public OwnedBooks(string bookId, string title, string description, string authorLastName) : base(bookId, title, description, authorLastName)
+        {
+            BookId = bookId;
+            Title = title;
+            Description = description;
+            AuthorLastName = authorLastName;
+        }
+
+        public OwnedBooks(Book book, bool owned, DateTime dateBought) // add book object to constructor
+        {
+            BookId = book.BookId;
+            Title = book.Title;
+            Description = book.Description;
+            AuthorLastName = book.AuthorLastName;
+            Owned = owned;
+            DateBought = dateBought;
+        }
+        public OwnedBooks(string bookId, string title, string description, string authorLastName, bool owned, DateTime dateBought) : base(bookId, title, description, authorLastName)
+        {
+            BookId = bookId;
+            Title = title;
+            Description = description;
+            AuthorLastName = authorLastName;
+            Owned = owned;
             DateBought = dateBought;
         }
 
