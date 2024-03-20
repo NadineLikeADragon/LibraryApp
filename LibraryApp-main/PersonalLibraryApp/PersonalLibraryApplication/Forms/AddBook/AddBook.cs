859c4b6941d5c77e638c06e13bdabd570700366b
Kerpackie <ckeleo@pm.me>
2024-03-20 17:00:09 +0000

    ReAdd Add Book Form
    Readds the AddBook Form.
    
    Note TODO: Fix the background image.

---
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Forms/AddBook/AddBook.cs
@@ -0,0 +1,52 @@
+﻿using PersonalLibraryApplication.Models;
+
+namespace PersonalLibraryApplication.Forms.AddBook;
+
+public partial class AddBook : Form
+{
+	private Book book = new Book();
+	public AddBook()
+	{
+		InitializeComponent();
+	}
+
+	private void button2_Click(object sender, EventArgs e)
+	{
+		this.Hide();
+	}
+
+	private void button1_Click(object sender, EventArgs e)
+	{
+		if (ValidateForm())
+		{
+			book = new Book(txtAuthorFName.Text, txtAuthorSName.Text, txtTitle.Text, txtDescription.Text);
+
+			this.Hide();
+		}
+	}
+
+	internal Book GetBook()
+	{
+		return book;
+	}
+
+	private bool ValidateForm()
+	{
+		if (txtTitle.Text == "")
+		{
+			MessageBox.Show("Please enter the title");
+			return false;
+		}
+		if (txtAuthorFName.Text == "" || txtAuthorSName.Text == "")
+		{
+			MessageBox.Show("Please enter the author name");
+			return false;
+		}
+		if (txtDescription.Text == "")
+		{
+			MessageBox.Show("Please describe the book");
+			return false;
+		}
+		return true;
+	}
+}
