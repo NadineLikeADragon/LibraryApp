7b32f560813673a6aed5c5e7c91cd48e7adf5fd8
Kerpackie <ckeleo@pm.me>
2024-03-20 17:59:08 +0000

    ReAdd AddLoan Forms
    ReAdds the AddLoan Forms.

---
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Forms/AddLoan/AddLoan.cs
@@ -0,0 +1,86 @@
+﻿using PersonalLibraryApplication.Models;
+
+namespace PersonalLibraryApplication.Forms.AddLoan;
+
+public partial class AddLoan : Form
+{
+    internal LoanTracking loan = new LoanTracking();
+    public bool IsAdd { get; set; }
+    public AddLoan()
+    {
+        InitializeComponent();
+    }
+
+    private void AddLoan_Load(object sender, EventArgs e)
+    {
+        if (IsAdd)
+        {
+            txtBookID.Enabled = true;
+        }
+        else
+        {
+            txtBookID.Enabled = false;
+            SetLoanFields(loan);
+        }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (ValidateForm())
+        {
+            loan = new LoanTracking(txtBookID.Text, txtTitle.Text, txtDescription.Text, txtAuthorSName.Text, dtpDateBorrowed.Value, dtpDueDate.Value);
+
+            this.Hide();
+        }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+    }
+
+    private bool ValidateForm()
+    {
+        if (txtTitle.Text == "")
+        {
+            MessageBox.Show("Please enter the title");
+            return false;
+        }
+        if (txtBookID.Text == "")
+        {
+            MessageBox.Show("Please enter the book ID");
+            return false;
+        }
+        if (txtAuthorSName.Text == "")
+        {
+            MessageBox.Show("Please enter the author name");
+            return false;
+        }
+        if (txtDescription.Text == "")
+        {
+            MessageBox.Show("Please describe the book");
+            return false;
+        }
+        if (dtpDateBorrowed.Value > dtpDueDate.Value)
+        {
+            MessageBox.Show("The due date must be after the date borrowed");
+            return false;
+        }
+        if (dtpDateBorrowed.Value > DateTime.Now)
+        {
+            MessageBox.Show("The date borrowed must be in the past");
+            return false;
+        }
+        return true;
+    }
+
+    private void SetLoanFields(LoanTracking loan)
+    {
+        txtBookID.Text = loan.BookId;
+        txtTitle.Text = loan.Title;
+        txtDescription.Text = loan.Description;
+        txtAuthorSName.Text = loan.AuthorLastName;
+        dtpDateBorrowed.Value = loan.DateBorrowed;
+        dtpDueDate.Value = loan.DueDate;
+    }
+}
