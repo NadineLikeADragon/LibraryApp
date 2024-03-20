01818d019ab2ea81f9f501a186d014d478f9fec9
Kerpackie <ckeleo@pm.me>
2024-03-20 17:40:24 +0000

    Refactor AddOwnedBook - Ref: Ref: f343b04
    Refactors AddOwnedBook to use Constructors, refer to #f343b04

--- LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Forms/AddOwnedBook/AddOwnedBook.cs
+++ LibraryApp-main/PersonalLibraryApp/PersonalLibraryApplication/Forms/AddOwnedBook/AddOwnedBook.cs
@@ -3,78 +3,91 @@
 namespace PersonalLibraryApplication.Forms.AddOwnedBook;
 
 public partial class AddOwnedBook : Form
+{
+    private Book book = new Book();
+    private OwnedBooks ownedBook = new OwnedBooks();
+    public AddOwnedBook()
     {
-        private Book book = new Book();
-        private OwnedBooks ownedBook = new OwnedBooks();
-        public AddOwnedBook()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void rbtnNewBook_CheckedChanged(object sender, EventArgs e)
-        {
-            panel1.Enabled = true;
-            txtDescription.Enabled = true;
-        }
+    private void rbtnNewBook_CheckedChanged(object sender, EventArgs e)
+    {
+        panel1.Enabled = true;
+        txtDescription.Enabled = true;
+    }
 
-        private void rbtnExistingBook_CheckedChanged(object sender, EventArgs e)
-        {
-            panel1.Enabled = false;
-            txtDescription.Enabled = false;
-        }
+    private void rbtnExistingBook_CheckedChanged(object sender, EventArgs e)
+    {
+        panel1.Enabled = false;
+        txtDescription.Enabled = false;
+    }
 
-        private void button1_Click(object sender, EventArgs e)
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (rbtnExistingBook.Checked)
         {
-            if (rbtnExistingBook.Checked)
+            if (ValidateForm())
             {
-                if (ValidateForm())
-                {
-                    ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
-                    this.Hide();
-                }
-            }
-            else
-            {
-                if (ValidateForm())
-                {
-                    book = new Book(txtAuthorFName.Text, txtAuthorSName.Text, txtTitle.Text, txtDescription.Text);
-                    ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
-                }
+                ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
+                this.Hide();
             }
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        else
         {
-            this.Hide();
-        }
-
-        internal OwnedBooks GetOwnedBook()
-        {
-            return ownedBook;
-        }
-
-        private bool ValidateForm()
-        {
-            if (txtTitle.Text == "")
+            if (ValidateForm())
             {
-                MessageBox.Show("Please enter the title");
-                return false;
+                book = new Book(txtBookID.Text, txtTitle.Text, txtDescription.Text, txtAuthorSName.Text);
+                ownedBook = new OwnedBooks(book, true, dtpDateBought.Value);
             }
-            if (txtAuthorFName.Text == "" || txtAuthorSName.Text == "")
-            {
-                MessageBox.Show("Please enter the author name");
-                return false;
-            }
-            if (txtDescription.Text == "")
-            {
-                MessageBox.Show("Please describe the book");
-                return false;
-            }
-            if (dtpDateBought.Value > DateTime.Now)
-            {
-                MessageBox.Show("Please enter a valid date");
-                return false;
-            }
-            return true;
         }
     }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+    }
+
+    internal OwnedBooks GetOwnedBook()
+    {
+        return ownedBook;
+    }
+
+    private bool ValidateForm()
+    {
+        if (txtTitle.Text == "")
+        {
+            MessageBox.Show("Please enter the title");
+            return false;
+        }
+        if (txtBookID.Text == "")
+        {
+            MessageBox.Show("Please enter the book ID");
+            return false;
+        }
+        if (txtAuthorSName.Text == "")
+        {
+            MessageBox.Show("Please enter the author name");
+            return false;
+        }
+        if (txtDescription.Text == "")
+        {
+            MessageBox.Show("Please describe the book");
+            return false;
+        }
+        if (dtpDateBought.Value > DateTime.Now)
+        {
+            MessageBox.Show("Date bought cannot be in the future");
+            return false;
+        }
+        return true;
+    }
+
+    private void SetBookFields(Book book)
+    {
+        txtBookID.Text = book.BookId;
+        txtTitle.Text = book.Title;
+        txtDescription.Text = book.Description;
+        txtAuthorSName.Text = book.AuthorLastName;
+    }
+}
